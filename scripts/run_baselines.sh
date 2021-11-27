d1058fb5a9bf469c3fbb8594bcdd27ee56960e0f
yangarbiter <yangarbiter@gmail.com>
2021-11-27 15:06:25 -0800

    Change batchsize for baselines

--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -3,22 +3,23 @@
 ROOTDIR="data"
 MODEL="vgg11"
 EPOCHS="50"
+BATCHSIZE="32"
 
 DATASET="celebA"
 
 mkdir -p ./logs/${DATASET}
 
 python examples/run_expt.py \
-  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --root_dir $ROOTDIR \
+  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
   --log_dir ./logs/${DATASET}/erm \
   --algorithm ERM 
 
 #python examples/run_expt.py \
-#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --root_dir $ROOTDIR \
+#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
 #  --log_dir ./logs/${DATASET}/erm_reweight \
 #  --algorithm ERM --uniform_over_groups
 
 #python examples/run_expt.py \
-#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --root_dir $ROOTDIR \
+#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
 #  --log_dir ./logs/${DATASET}/groupDRO \
 #  --algorithm groupDRO
