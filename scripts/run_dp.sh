5ecc24135384863eba41e4ca4cb13226e506f56a
yangarbiter <yangarbiter@gmail.com>
2021-11-28 12:26:44 -0800

    update DPSGD

--- scripts/run_dp.sh
+++ scripts/run_dp.sh
@@ -9,12 +9,12 @@ DATASET="celebA"
 
 mkdir -p ./logs/${DATASET}
 
-python examples/run_expt.py \
-  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-  --optimizer DPSGD --delta 1e-5 --sigma 1.0 --max_per_sample_grad_norm 1.0 \
-  --uniform_iid --sample_rate 0.0005 \
-  --log_dir ./logs/${DATASET}/erm-dpsgd_1e-5_1.0_1.0_0.0005 \
-  --algorithm ERM --download
+#python examples/run_expt.py \
+#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+#  --optimizer DPSGD --delta 1e-5 --sigma 1.0 --max_per_sample_grad_norm 1.0 \
+#  --uniform_iid --sample_rate 0.0005 \
+#  --log_dir ./logs/${DATASET}/erm-dpsgd_1e-5_1.0_1.0_0.0005 \
+#  --algorithm ERM --download
 
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
@@ -23,9 +23,9 @@ python examples/run_expt.py \
 #  --log_dir ./logs/${DATASET}/erm-dpsgd_1e-5_2.0_1.0_0.0005 \
 #  --algorithm ERM
 
-#python examples/run_expt.py \
-#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-#  --optimizer DPSGD --delta 1e-5 --sigma 1.0 --max_per_sample_grad_norm 1.0 \
-#  --weighted_uniform_iid --sample_rate 0.0005 \
-#  --log_dir ./logs/${DATASET}/erm-dpsgd_1e-5_1.0_1.0_0.0005 \
-#  --algorithm ERM
+python examples/run_expt.py \
+  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+  --optimizer DPSGD --delta 1e-5 --sigma 1.0 --max_per_sample_grad_norm 1.0 \
+  --weighted_uniform_iid --sample_rate 0.00025 \
+  --log_dir ./logs/${DATASET}/erm-dpsgd_1e-5_1.0_1.0_0.00025 \
+  --algorithm ERM
