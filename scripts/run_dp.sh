08d9ace7eb31002426de90436235c4b52d65e382
yangarbiter <yangarbiter@gmail.com>
2021-12-04 09:04:35 -0800

    update script

--- scripts/run_dp.sh
+++ scripts/run_dp.sh
@@ -11,37 +11,31 @@ MODEL="dp_resnet50"
 BATCHSIZE="32"
 DATASET="waterbirds"
 EPOCHS="300"
-SAMPLERATE="0.001"
+SAMPLERATE="0.002"
+SIGMA="1.0"
 
 mkdir -p ./logs/${DATASET}
 
 # ERM + DPSGD
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-#  --optimizer DPSGD --delta 1e-5 --sigma 1.0 --max_per_sample_grad_norm 1.0 \
+#  --optimizer DPSGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm 1.0 \
 #  --uniform_iid --sample_rate $SAMPLERATE --weight_decay 0. \
-#  --log_dir ./logs/${DATASET}/erm-${MODEL}-dpsgd_1e-5_1.0_1.0_${SAMPLERATE} \
+#  --log_dir ./logs/${DATASET}/erm-${MODEL}-dpsgd_1e-5_${SIGMA}_1.0_${SAMPLERATE} \
 #  --algorithm ERM --download
-
-#python examples/run_expt.py \
-#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-#  --optimizer DPSGD --delta 1e-5 --sigma 2.0 --max_per_sample_grad_norm 1.0 \
-#  --uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. \
-#  --log_dir ./logs/${DATASET}/erm-${MODEL}-dpsgd_1e-5_2.0_1.0_${SAMPLERATE} \
-#  --algorithm ERM
 
 # weighted + DPSGD
 python examples/run_expt.py \
   --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-  --optimizer DPSGD --delta 1e-5 --sigma 1.0 --max_per_sample_grad_norm 1.0 \
+  --optimizer DPSGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm 1.0 \
   --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. \
-  --log_dir ./logs/${DATASET}/weightederm-${MODEL}-dpsgd_1e-5_1.0_1.0_${SAMPLERATE} \
+  --log_dir ./logs/${DATASET}/weightederm-${MODEL}-dpsgd_1e-5_${SIGMA}_1.0_${SAMPLERATE} \
   --algorithm ERM
 
 # DRO + DPSGD
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-#  --optimizer DPSGD --delta 1e-5 --sigma 1.0 --max_per_sample_grad_norm 1.0 \
+#  --optimizer DPSGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm 1.0 \
 #  --uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. \
-#  --log_dir ./logs/${DATASET}/groupdro-${MODEL}-dpsgd_1e-5_1.0_1.0_${SAMPLERATE} \
+#  --log_dir ./logs/${DATASET}/groupdro-${MODEL}-dpsgd_1e-5_${SIGMA}_1.0_${SAMPLERATE} \
 #  --algorithm groupDRO --download
