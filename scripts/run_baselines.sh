520583c581f8a32b226262691dfa77c2deef3f50
yangarbiter <yangarbiter@gmail.com>
2021-12-04 18:57:23 -0800

    update run_baselines with civialcomments

--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -8,31 +8,36 @@ DATASET="celebA"
 EPOCHS="50"
 
 MODEL="resnet50"
-BATCHSIZE="128"
+BATCHSIZE="64"
 DATASET="waterbirds"
 EPOCHS="300"
 
+MODEL="distilbert-base-uncased"
+BATCHSIZE="16"
+DATASET="civilcomments"
+EPOCHS="5"
+
 mkdir -p ./logs/${DATASET}
 
-python examples/run_expt.py \
-  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-  --log_dir ./logs/${DATASET}/erm-${MODEL} \
-  --algorithm ERM --weight_decay 0.
+#python examples/run_expt.py \
+#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+#  --log_dir ./logs/${DATASET}/erm-${MODEL} \
+#  --algorithm ERM --weight_decay 0. --download
 
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
 #  --log_dir ./logs/${DATASET}/erm_reweight-${MODEL} \
-#  --algorithm ERM --uniform_over_groups --weight_decay 0.
+#  --algorithm ERM --uniform_over_groups --weight_decay 0. --download
 
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
 #  --log_dir ./logs/${DATASET}/groupDRO-${MODEL} \
 #  --algorithm groupDRO --weight_decay 0.
 
-#python examples/run_expt.py \
-#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-#  --log_dir ./logs/${DATASET}/groupDRO-${MODEL}_wd0.1 \
-#  --algorithm groupDRO --weight_decay 0.1
+python examples/run_expt.py \
+  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+  --log_dir ./logs/${DATASET}/groupDRO-${MODEL}_wd0.1 \
+  --algorithm groupDRO --weight_decay 0.1
 
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
