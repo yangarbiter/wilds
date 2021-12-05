7be2c402df83b30d3a5892378faf966b08ebc499
yangarbiter <yangarbiter@gmail.com>
2021-12-04 21:29:11 -0800

    Add civialcomments to dp script

--- scripts/run_dp.sh
+++ scripts/run_dp.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
 ROOTDIR="data"
-MODEL="dp_resnet18"
-BATCHSIZE="8"
-DATASET="celebA"
-EPOCHS="50"
-SAMPLERATE="0.00025"
 
-MODEL="dp_resnet50"
-BATCHSIZE="32"
-DATASET="waterbirds"
-EPOCHS="300"
+#MODEL="dp_resnet18"
+#BATCHSIZE="8"
+#DATASET="celebA"
+#EPOCHS="50"
+#SAMPLERATE="0.00025"
+#SIGMA="1.0"
+
+#MODEL="dp_resnet50"
+#BATCHSIZE="32"
+#DATASET="waterbirds"
+#EPOCHS="300"
+#SAMPLERATE="0.002"
+#SIGMA="0.5"
+
+MODEL="distilbert-base-uncased"
+BATCHSIZE="16"
+DATASET="civilcomments"
+EPOCHS="5"
 SAMPLERATE="0.002"
-SIGMA="1.0"
+SIGMA="0.5"
 
 mkdir -p ./logs/${DATASET}
 
@@ -31,6 +40,7 @@ python examples/run_expt.py \
   --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. \
   --log_dir ./logs/${DATASET}/weightederm-${MODEL}-dpsgd_1e-5_${SIGMA}_1.0_${SAMPLERATE} \
   --algorithm ERM
+
 
 # DRO + DPSGD
 #python examples/run_expt.py \
