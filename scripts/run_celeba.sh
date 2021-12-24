a3df397076b07982766b9938b30fb9f4642a9507
yangarbiter <yangarbiter@gmail.com>
2021-12-24 10:02:44 -0800

    update

--- scripts/run_celeba.sh
+++ scripts/run_celeba.sh
@@ -15,16 +15,34 @@ mkdir -p ./logs/${DATASET}
 #############################
 # weighted + DPSGD
 #############################
+#for CLIPNORM in 1.0
+#do
+#  for SIGMA in 0.1 1.0
+#  do
+#    PYTHONPATH=. python examples/run_expt.py \
+#      --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+#      --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm $CLIPNORM --enable_privacy \
+#      --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. \
+#      --log_dir ./logs/${DATASET}/weightederm-${MODEL}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
+#      --algorithm ERM
+#  done
+#done
+
+#############################
+# ERM + DPSGD
+#############################
+LR="1e-3"
+SAMPLERATE="0.0001"
 for CLIPNORM in 1.0
 do
-  for SIGMA in 0.1 1.0
+  for SIGMA in 0.0001 0.001 0.01 0.1 1.0 10.0
   do
-    PYTHONPATH=. python examples/run_expt.py \
+    python examples/run_expt.py \
       --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
       --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm $CLIPNORM --enable_privacy \
-      --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. \
-      --log_dir ./logs/${DATASET}/weightederm-${MODEL}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
-      --algorithm ERM
+      --uniform_iid --sample_rate $SAMPLERATE --weight_decay 0. --lr ${LR} \
+      --log_dir ./logs/${DATASET}/erm-${MODEL}-lr${LR}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
+      --algorithm ERM --download
   done
 done
 
@@ -41,13 +59,6 @@ done
 #    --algorithm groupDRO --download
 #done
 
-# ERM + DPSGD
-#python examples/run_expt.py \
-#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-#  --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm $CLIPNORM --enable_privacy \
-#  --uniform_iid --sample_rate $SAMPLERATE --weight_decay 0. \
-#  --log_dir ./logs/${DATASET}/erm-${MODEL}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
-#  --algorithm ERM --download
 
 # IWERM + DPSGD
 #python examples/run_expt.py \
