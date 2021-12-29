35f6da65c9673d7921ccdb08738cb8e46269c32d
yangarbiter <yangarbiter@gmail.com>
2021-12-28 20:54:01 -0800

    update

--- scripts/run_utkface.sh
+++ scripts/run_utkface.sh
@@ -13,19 +13,19 @@ mkdir -p ./logs/${DATASET}
 #############################
 # IWERM + NoiseSGD
 #############################
-MODEL="resnet50"
-for LR in 1e-3
-do
-  for SIGMA in 0.0001 0.1 10.0
-  do
-    python examples/run_expt.py \
-      --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-      --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --apply_noise \
-      --weight_decay 0. --lr ${LR} \
-      --log_dir ./logs/${DATASET}/iwerm-${MODEL}-lr${LR}-noisesgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
-      --algorithm IWERM --download
-  done
-done
+#MODEL="resnet50"
+#for LR in 1e-3
+#do
+#  for SIGMA in 0.01 1.0
+#  do
+#    python examples/run_expt.py \
+#      --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+#      --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --apply_noise \
+#      --weight_decay 0. --lr ${LR} \
+#      --log_dir ./logs/${DATASET}/iwerm-${MODEL}-lr${LR}-noisesgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
+#      --algorithm IWERM --download
+#  done
+#done
 
 #############################
 # ERM + DPSGD
@@ -51,8 +51,8 @@ done
 #############################
 # DPSGD IW
 #############################
-#MODEL="dp_resnet50"
-#SAMPLERATE=0.001
+MODEL="dp_resnet50"
+SAMPLERATE=0.001
 #for CLIPNORM in 0.1 10.0
 #do
 #  for SIGMA in 0.001 0.01 0.1 1.0
@@ -66,6 +66,22 @@ done
 #  done
 #done
 #
+for LR in 1e-3
+do
+  for CLIPNORM in 10.0 1.0
+  do
+    for SIGMA in 0.000001 0.00001 0.0001
+    do
+      PYTHONPATH=. python examples/run_expt.py \
+        --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+        --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm $CLIPNORM --enable_privacy \
+        --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. --lr ${LR} \
+        --log_dir ./logs/${DATASET}/weightederm-${MODEL}-lr${LR}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
+        --algorithm ERM --download
+    done
+  done
+done
+
 #for LR in 1e-2 1e-4
 #do
 #  for CLIPNORM in 1.0
@@ -82,28 +98,28 @@ done
 #  done
 #done
 
-MODEL="dp_resnet50"
-#SAMPLERATE=0.001
-SAMPLERATE=0.002
-for LR in 1e-3
-do
-  for CLIPWEIGHT in 0.001 0.002
-  do
-    for CLIPNORM in 1.0
-    do
-      for SIGMA in 0.01 0.1 1.0
-      do
-        PYTHONPATH=. python examples/run_expt.py \
-          --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
-          --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm $CLIPNORM --enable_privacy \
-          --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. --lr ${LR} \
-          --clip_sample_rate $CLIPWEIGHT \
-          --log_dir ./logs/${DATASET}/weightederm-cw${CLIPWEIGHT}-${MODEL}-lr${LR}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
-          --algorithm ERM --download
-      done
-    done
-  done
-done
+#MODEL="dp_resnet50"
+##SAMPLERATE=0.001
+#SAMPLERATE=0.002
+#for LR in 1e-3
+#do
+#  for CLIPWEIGHT in 0.001 0.002
+#  do
+#    for CLIPNORM in 1.0
+#    do
+#      for SIGMA in 0.01 0.1 1.0
+#      do
+#        PYTHONPATH=. python examples/run_expt.py \
+#          --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+#          --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm $CLIPNORM --enable_privacy \
+#          --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. --lr ${LR} \
+#          --clip_sample_rate $CLIPWEIGHT \
+#          --log_dir ./logs/${DATASET}/weightederm-cw${CLIPWEIGHT}-${MODEL}-lr${LR}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
+#          --algorithm ERM --download
+#      done
+#    done
+#  done
+#done
 
 #
 #for CLIPNORM in 0.1 10.0
