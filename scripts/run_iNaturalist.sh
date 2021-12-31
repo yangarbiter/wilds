d02a68113f4ec1f0dc80dfba5c79401cc6aa1126
yangarbiter <yangarbiter@gmail.com>
2021-12-31 14:39:18 -0800

    update

--- scripts/run_iNaturalist.sh
+++ scripts/run_iNaturalist.sh
@@ -13,12 +13,22 @@ LR="1e-3"
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
 #  --log_dir ./logs/${DATASET}/erm-${MODEL}-lr${LR} \
-#  --algorithm ERM --weight_decay 0. --download
+#  --algorithm ERM --weight_decay 0. --lr ${LR} --download
 
 #python examples/run_expt.py \
 #  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
 #  --log_dir ./logs/${DATASET}/iwerm-${MODEL}-lr${LR} \
-#  --algorithm IWERM --weight_decay 0. --download
+#  --algorithm IWERM --weight_decay 0. --lr ${LR} --download
+
+#python examples/run_expt.py \
+#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+#  --log_dir ./logs/${DATASET}/groupDRO-${MODEL}-lr${LR} \
+#  --algorithm groupDRO --weight_decay 0. --lr ${LR} --download
+
+#python examples/run_expt.py \
+#  --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
+#  --log_dir ./logs/${DATASET}/groupDRO-${MODEL}-lr${LR}_wd0.1 \
+#  --algorithm groupDRO --weight_decay 0.1 --lr ${LR} --download
 
 
 #############################
@@ -28,14 +38,14 @@ MODEL="dp_resnet18"
 SAMPLERATE=0.0001
 LR="1e-3"
 BATCHSIZE="64"
-for CLIPNORM in 1.0
+for CLIPNORM in 100.0
 do
-  for SIGMA in 0.0001
+  for SIGMA in 0.00000001
   do
     PYTHONPATH=. python examples/run_expt.py \
       --dataset $DATASET --model $MODEL --n_epochs $EPOCHS --batch_size $BATCHSIZE --root_dir $ROOTDIR \
       --optimizer SGD --delta 1e-5 --sigma ${SIGMA} --max_per_sample_grad_norm $CLIPNORM --enable_privacy \
-      --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. \
+      --weighted_uniform_iid --sample_rate ${SAMPLERATE} --weight_decay 0. --lr ${LR} \
       --log_dir ./logs/${DATASET}/weightederm-${MODEL}-dpsgd_1e-5_${SIGMA}_${CLIPNORM}_${SAMPLERATE} \
       --algorithm ERM --download
   done
