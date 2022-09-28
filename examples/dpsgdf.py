69890ea9e360a0755eacb07d1f0093acee945cd5
yangarbiter <yangarbiter@gmail.com>
2022-09-28 08:29:02 -0700

    update

--- examples/dpsgdf.py
+++ examples/dpsgdf.py
@@ -208,39 +208,6 @@ class FairPrivacyEngine(PrivacyEngine):
         distributed: bool = False,
         clipping: str = "flat",
         noise_generator=None,
-    ) -> DPOptimizer:
-        if isinstance(optimizer, DPOptimizer):
-            optimizer = optimizer.original_optimizer
-
-        generator = None
-        if self.secure_mode:
-            generator = self.secure_rng
-        elif noise_generator is not None:
-            generator = noise_generator
-
-        optim_class = FairDPOptimizer
-
-        return optim_class(
-            optimizer=optimizer,
-            noise_multiplier=noise_multiplier,
-            max_grad_norm=max_grad_norm,
-            expected_batch_size=expected_batch_size,
-            loss_reduction=loss_reduction,
-            generator=generator,
-            secure_mode=self.secure_mode,
-        )
-
-    def _prepare_optimizer(
-        self,
-        optimizer: optim.Optimizer,
-        *,
-        noise_multiplier: float,
-        max_grad_norm: Union[float, List[float]],
-        expected_batch_size: int,
-        loss_reduction: str = "mean",
-        distributed: bool = False,
-        clipping: str = "flat",
-        noise_generator=None,
         group_info,
         n_groups,
         C0: float,
